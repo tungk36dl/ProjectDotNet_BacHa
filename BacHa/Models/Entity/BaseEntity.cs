77c36620d66ec93346430e7d4c77d93178c71786
Pham The Tung <tungxeco55@gmail.com>
2025-09-18 15:09:48 +0700

    update

--- BacHa/Models/Entity/BaseEntity.cs
+++ BacHa/Models/Entity/BaseEntity.cs
@@ -3,7 +3,9 @@
     public class BaseEntity : DomainEntity<Guid>
     {
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public Guid? CreatedBy { get; set; }
         public DateTime? UpdatedAt { get; set; }
+        public Guid? UpdatedBy { get; set;}
     }
 }
 
